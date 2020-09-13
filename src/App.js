d31ad5dad64ef38f81fb0a5c3766f5714a10adfe
Natsuki <nkondo3@gmail.com>
2020-09-13 23:58:49 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [count, setCount] = useState(3)
+  const [theme, setTheme] = useState('個です')
+
+  function decrementCount() {
+    if (count <= 0) {
+      return
+    }
+    setCount(prevCount => prevCount - 1)
+    // setCount(prevState => {
+    //   return { ...prevState, count: prevState.count - 1}
+    // })
+    setTheme('個に減ったよ😓')
+  }
+
+  function increamentCount() {
+    setCount(prevCount => prevCount + 1)
+    setTheme('個に増えたよ🥰')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h1>How much do you have?</h1>
+      <div className='counter-text'>
+        <div className='count'>{count}</div>
+        <div className='theme' >{theme}</div>
+      </div>
+      <button className='btn d-btn' onClick={decrementCount}>-</button>
+      <button className='btn i-btn' onClick={increamentCount}>+</button>
     </div>
   );
 }
